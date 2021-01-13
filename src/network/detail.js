f57b37f8e3a3300937b4251a1e2baec4d724188e
fuhua-ren <1208716270@qq.com>
2021-01-13 14:49:47 +0800

    变更提交

--- src/network/detail.js
+++ src/network/detail.js
@@ -8,3 +8,18 @@ export function getDetail(iid) {
 		}
 	})
 }
+
+
+// 现在的请求方式默认是get
+// 如果要换成post等 这加上method: 'POST', 
+// 而且不然传入params而是要传入data
+// data:{key:'aa'}
+// 如：
+
+// export function getLogin(data) {
+// 	return request({
+// 		method: 'POST',
+// 		url: '/Login',
+// 		data
+// 	})
+// }
--- src/network/request.js
+++ src/network/request.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export function request(config) {
   // 1.创建axios的实例
+  // create创建出来的实例返回是一个promise
   const instance = axios.create({
     baseURL: 'http://152.136.185.210:8000/api/w6',
     timeout: 5000
@@ -10,6 +11,8 @@ export function request(config) {
   // 2.axios的拦截器
   // 2.1.请求拦截的作用
   instance.interceptors.request.use(config => {
+    // 拦截器主要操作， 1，处理不符合的信息 2.请求图标 3.token处理
+
     return config
   }, err => {
     // console.log(err);
